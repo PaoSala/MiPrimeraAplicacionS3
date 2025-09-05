fa81b964bc484f0e180ed9b23234fb8afbbf5e1f
PaoSala <pa.sala@duocuc.cl>
2025-09-05 11:41:54 -0400

    Se añadio try, catch, operaciones matematicas, int, evitar que numeros sean 0 si es que estan divididos

--- app/src/main/java/com/example/miprimeraaplicacin/MainActivity3.kt
+++ app/src/main/java/com/example/miprimeraaplicacin/MainActivity3.kt
@@ -3,27 +3,37 @@ package com.example.miprimeraaplicacin
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
+import com.example.miprimeraaplicacin.funciones.OpMatematicas
 
-class MainActivity2 : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
-        val txUsuario:TextView =findViewById(R.id.id_usuario_loggeado)
-        val recibirUsernameS = intent.getStringExtra("par_usern")
-        val btnGoCalculadora : Button = findViewById(R.id.btn_calculadora)
+        setContentView(R.layout.activity_main3)
 
-        txUsuario.text = recibirUsernameS.toString()
-        btnGoCalculadora.setOnClickListener {
-            val nuevaVentana2 = Intent(this, MainActivity3::class.java)
-            startActivity(nuevaVentana2)
+        val ednumcal1: EditText = findViewById(R.id.ed_num1)
+        val ednumcal2: EditText = findViewById(R.id.ed_num2)
+        val btnCalc: Button = findViewById(R.id.btncalcular)
+        val txNumCalc: TextView = findViewById(R.id.txtNumRec)
+
+
+
+        btnCalc.setOnClickListener {
+            var numeroUno: Int = ednumcal1.text.toString().toIntOrNull() ?: 0
+            var numeroDos: Int = ednumcal2.text.toString().toIntOrNull() ?: 0
+
+            txNumCalc.text = OpMatematicas.dividirValores(numeroUno, numeroDos)
+
+            // txMensaje.text = edUsername.text.toString()
         }
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
